ca7e35415f2ee7425b9e6f2731b7ebe141ff903b
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2024-09-26 11:04:52 +0700

    Add unit test example

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,6 +8,8 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      imports: [],
+      providers: [],
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -14,5 +17,21 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Bay', 'U');
+    expect(response).toBe('Hello Bay U');
+  });
+
+  it('should can view template', async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('Bay', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      title: 'Template Engine',
+      name: 'Bay',
+    });
   });
 });
